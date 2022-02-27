55750c2cd4e6b68c44a668ce51ac20b58e1bab54
coderzry <18348738308@163.com>
2022-02-27 16:47:39 +0800

    chore(config): 集成vueRouter和vuex

---
+++ src/store/index.ts
@@ -0,0 +1,14 @@
+import { createStore } from 'vuex'
+
+const store = createStore({
+  state: () => {
+    return {
+      name: 'coderzry'
+    }
+  },
+  mutations: {},
+  getters: {},
+  actions: {}
+})
+
+export default store
